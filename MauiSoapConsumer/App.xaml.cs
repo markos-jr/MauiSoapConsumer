cc5007959f0eac3be567fa41d9dc0f8337b7dfe6
Marcos G. Júnior <markos-jr@outlook.com>
2024-07-03 22:36:14 -0300

    Add project files.

---
+++ MauiSoapConsumer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiSoapConsumer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
