3ae232dc0a305a20b3b22482213b58a211696c06
Marcos G. Júnior <markos-jr@outlook.com>
2024-07-05 00:13:17 -0300

    feat: consumir serviço CalculatorSoap para realizar cálculos na tela inicia

--- MauiSoapConsumer/MainPage.xaml.cs
+++ MauiSoapConsumer/MainPage.xaml.cs
@@ -1,14 +1,47 @@
-﻿namespace MauiSoapConsumer
+﻿using WsCalculator;
+namespace MauiSoapConsumer
 {
     public partial class MainPage : ContentPage
     {
-
+        private CalculatorSoapClient _service;
         public MainPage()
         {
             InitializeComponent();
+
+            _service = new CalculatorSoapClient(new CalculatorSoapClient.EndpointConfiguration());
         }
 
-        
+        private async void Somar(object sender, EventArgs e)
+        {
+            int.TryParse(Number1.Text, out int numb1);
+            int.TryParse(Number2.Text, out int numb2);
+
+            LblResult.Text = (await _service.AddAsync(numb1, numb2)).ToString();
+        }
+
+        private async void Subtrair(object sender, EventArgs e)
+        {
+            int.TryParse(Number1.Text, out int numb1);
+            int.TryParse(Number2.Text, out int numb2);
+
+            LblResult.Text = (await _service.SubtractAsync(numb1, numb2)).ToString();
+        }
+
+        private async void Dividir(object sender, EventArgs e)
+        {
+            int.TryParse(Number1.Text, out int numb1);
+            int.TryParse(Number2.Text, out int numb2);
+
+            LblResult.Text = (await _service.DivideAsync(numb1, numb2)).ToString();
+        }
+
+        private async void Multiplicar(object sender, EventArgs e)
+        {
+            int.TryParse(Number1.Text, out int numb1);
+            int.TryParse(Number2.Text, out int numb2);
+
+            LblResult.Text = (await _service.MultiplyAsync(numb1, numb2)).ToString();
+        }
     }
 
 }
